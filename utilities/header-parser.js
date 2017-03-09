6d2d9e41ade40a6827933596e79c8f402c10bc12
William <thenecroyeti@gmail.com>
2017-03-09 15:53:09 +1300

    fixed ip address bug

--- utilities/header-parser.js
+++ utilities/header-parser.js
@@ -1,5 +1,5 @@
 module.exports = (request) => {
-  const ipAddress = request.socket.remoteAddress;
+  const ipAddress = request.socket.remoteAddress.split(':').slice(-1);
   const software = request.headers['user-agent'].split(/[()]/)[1];
   const language = request.headers['accept-language'].split(',')[0];
 
