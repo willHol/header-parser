8343dd05c1c3adc222312d0d6677debcb0a155c4
William <thenecroyeti@gmail.com>
2017-03-09 15:50:27 +1300

    Conformed to linting requirements

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const headerParser = require('./utilities/header-parser');
+
 const app = express();
 
-app.get('/whoami', function(request, response) {
+app.get('/whoami', (request, response) => {
+  // Destructuring assignment allows for multiple return values
+  const [ipAddress, software, language] = headerParser(request);
 
-	// Destructuring assignment allows for multiple return values
-	let [ ipAddress, software, language ] = headerParser(request);
-
-	response.json({
-		'ip-address': ipAddress,
-		software,
-		language
-	});
+  response.json({
+    'ip-address': ipAddress,
+    software,
+    language,
+  });
 });
 
 module.exports = app.listen(process.env.PORT || 3000);
